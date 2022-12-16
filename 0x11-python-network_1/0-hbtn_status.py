db80f9dfd053f13ec0e3469ba4d4b6e2fcc0cd47
Samueldamil <damilareedun@gmail.com>
2022-12-16 13:13:10 +0000

    file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""A script that fetches https://alx-intranet.hbtn.io/status"""
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
 
 if __name__ == '__main__':
@@ -7,7 +10,7 @@ if __name__ == '__main__':
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
-        print("Body responce:")
+        print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
         print("\t- utf8 content: {}".format(content.decode('utf-8')))
